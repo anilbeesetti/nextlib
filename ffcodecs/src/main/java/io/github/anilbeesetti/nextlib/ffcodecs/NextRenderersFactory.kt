f24d77e68ceee39d19532852b039a3e117ed603d
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2023-09-03 15:59:54 +0530

    feat: add sync supported text renderer (#18)

--- ffcodecs/src/main/java/io/github/anilbeesetti/nextlib/ffcodecs/NextRenderersFactory.kt
+++ ffcodecs/src/main/java/io/github/anilbeesetti/nextlib/ffcodecs/NextRenderersFactory.kt
@@ -2,15 +2,26 @@ package io.github.anilbeesetti.nextlib.ffcodecs
 
 import android.content.Context
 import android.os.Handler
+import android.os.Looper
 import androidx.media3.common.util.Log
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.Renderer
 import androidx.media3.exoplayer.audio.AudioRendererEventListener
 import androidx.media3.exoplayer.audio.AudioSink
 import androidx.media3.exoplayer.mediacodec.MediaCodecSelector
+import androidx.media3.exoplayer.text.TextOutput
 import androidx.media3.exoplayer.video.VideoRendererEventListener
+import io.github.anilbeesetti.nextlib.text.NextTextRenderer
 
 class NextRenderersFactory(context: Context) : DefaultRenderersFactory(context) {
+
+    private var subtitleSyncOffsetUs = 0L
+
+    fun setSubtitleSyncOffsetUs(offsetUs: Long): NextRenderersFactory {
+        subtitleSyncOffsetUs = offsetUs
+        return this
+    }
+
     override fun buildAudioRenderers(
         context: Context,
         extensionRendererMode: Int,
@@ -85,6 +96,17 @@ class NextRenderersFactory(context: Context) : DefaultRenderersFactory(context) 
             // The extension is present, but instantiation failed.
             throw java.lang.RuntimeException("Error instantiating Ffmpeg extension", e)
         }
+    }
+
+    override fun buildTextRenderers(
+        context: Context,
+        output: TextOutput,
+        outputLooper: Looper,
+        extensionRendererMode: Int,
+        out: java.util.ArrayList<Renderer>
+    ) {
+        val renderer = NextTextRenderer(subtitleSyncOffsetUs, output, outputLooper)
+        out.add(renderer)
     }
 
     companion object {
