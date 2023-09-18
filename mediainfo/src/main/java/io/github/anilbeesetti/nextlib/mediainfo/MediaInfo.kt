15eaac86e5ca00211ed3b4b471bd618b1a3fb9b2
anilbeesetti <anilbeesetti10@gmail.com>
2023-09-18 13:09:46 +0530

    move media info sub data classes to separate files

--- mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/MediaInfo.kt
+++ mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/MediaInfo.kt
@@ -9,35 +9,3 @@ data class MediaInfo(
 )
 
 
-data class VideoStream(
-    val index: Int,
-    val title: String?,
-    val codecName: String,
-    val language: String?,
-    val disposition: Int,
-    val bitRate: Long,
-    val frameRate: Double,
-    val frameWidth: Int,
-    val frameHeight: Int
-)
-
-data class AudioStream(
-    val index: Int,
-    val title: String?,
-    val codecName: String,
-    val language: String?,
-    val disposition: Int,
-    val bitRate: Long,
-    val sampleFormat: String?,
-    val sampleRate: Int,
-    val channels: Int,
-    val channelLayout: String?
-)
-
-data class SubtitleStream(
-    val index: Int,
-    val title: String?,
-    val codecName: String,
-    val language: String?,
-    val disposition: Int
-)
