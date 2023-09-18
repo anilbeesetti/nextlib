ba487f47ff172edfcd1ad8461a3469e1fbdf4365
anilbeesetti <anilbeesetti10@gmail.com>
2023-09-18 14:22:05 +0530

    refactor: PathUtil object to separate file

--- mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/MediaInfoBuilder.kt
+++ mediainfo/src/main/java/io/github/anilbeesetti/nextlib/mediainfo/MediaInfoBuilder.kt
@@ -1,14 +1,8 @@
 package io.github.anilbeesetti.nextlib.mediainfo
 
-import android.annotation.SuppressLint
-import android.content.ContentUris
 import android.content.Context
-import android.database.Cursor
 import android.net.Uri
-import android.os.Environment
 import android.os.ParcelFileDescriptor
-import android.provider.DocumentsContract
-import android.provider.MediaStore
 import android.util.Log
 import androidx.annotation.Keep
 import java.io.FileNotFoundException
@@ -176,94 +170,6 @@ class MediaInfoBuilder(private val context: Context) {
 
     init {
         System.loadLibrary("mediainfo")
-    }
-}
-
-
-internal object PathUtil {
-    /*
-     * Gets the file path of the given Uri.
-     */
-    @SuppressLint("NewApi")
-    fun getPath(context: Context, uri: Uri): String? {
-        var uri = uri
-        var selection: String? = null
-        var selectionArgs: Array<String>? = null
-
-
-        if (DocumentsContract.isDocumentUri(context.applicationContext, uri)) {
-            if (isExternalStorageDocument(uri)) {
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
-            } else if (isDownloadsDocument(uri)) {
-                val id = DocumentsContract.getDocumentId(uri)
-                uri = if (id.startsWith("raw:/")) {
-                    return id.replace("raw:/", "file:///")
-                } else {
-                    if (id.startsWith("msf:")) {
-                        // Case: Android 10 emulator, a video file downloaded via Chrome app.
-                        // No knowledge how to reconstruct the file path. So just fail fast.
-                        return null
-                    }
-                    ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), id.toLong()
-                    )
-                }
-            } else if (isMediaDocument(uri)) {
-                val docId = DocumentsContract.getDocumentId(uri)
-                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                val type = split[0]
-                if ("image" == type) {
-                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                } else if ("video" == type) {
-                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                } else if ("audio" == type) {
-                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-                }
-                selection = "_id=?"
-                selectionArgs = arrayOf(split[1])
-            }
-        }
-        if ("content".equals(uri.scheme, ignoreCase = true)) {
-            val projection = arrayOf(MediaStore.Images.Media.DATA)
-            var cursor: Cursor? = null
-            try {
-                cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
-                val column_index = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                if (cursor.moveToFirst()) {
-                    return cursor.getString(column_index)
-                }
-            } catch (e: Exception) {
-            }
-        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
-            return uri.path
-        }
-        return null
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    fun isExternalStorageDocument(uri: Uri): Boolean {
-        return "com.android.externalstorage.documents" == uri.authority
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    fun isDownloadsDocument(uri: Uri): Boolean {
-        return "com.android.providers.downloads.documents" == uri.authority
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    fun isMediaDocument(uri: Uri): Boolean {
-        return "com.android.providers.media.documents" == uri.authority
     }
 }
 
