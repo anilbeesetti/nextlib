56bca7fb0a51f674a5daa248c7d23ad7fb1bc900
Anil Kumar Beesetti <66936649+anilbeesetti@users.noreply.github.com>
2024-05-19 11:43:10 +0530

    Feat: Intial FFmpeg av1 codec support with dav1d (#76)
    * add dav1d to ffmpeg
    
    * add libdav1d decoder to ffmpeg video renderer
    
    * add build tools to nextlib build workflow
    
    * update workflow build package name
    
    * set pkg config lib path
    
    * fix pkg config lib path

--- media3ext/src/main/java/io/github/anilbeesetti/nextlib/media3ext/ffdecoder/FfmpegLibrary.java
+++ media3ext/src/main/java/io/github/anilbeesetti/nextlib/media3ext/ffdecoder/FfmpegLibrary.java
@@ -124,6 +124,7 @@ public final class FfmpegLibrary {
       case MimeTypes.VIDEO_MPEG2 -> "mpeg2video";
       case MimeTypes.VIDEO_VP8 -> "libvpx";
       case MimeTypes.VIDEO_VP9 -> "libvpx-vp9";
+      case MimeTypes.VIDEO_AV1 -> "libdav1d";
       default -> null;
     };
   }
